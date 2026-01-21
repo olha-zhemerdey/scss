eeeaa71986224b4a4206786c17c95034177ac269
Olha <oyashnaya88@gmail.com>
2026-01-21 14:49:03 +0200

    om

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,48 @@
-import imagemin from "imagemin";
-import imageminWebp from "imagemin-webp";
-import path from "path";
-import { defineConfig } from "vite";
-import glob from "fast-glob";
-import { fileURLToPath } from "url";
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import imagemin from 'imagemin';
+import imageminWebp from 'imagemin-webp';
+import path from 'path';
+import { defineConfig } from 'vite';
+import glob from 'fast-glob';
+import { fileURLToPath } from 'url';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: '/scss/',
+
   plugins: [
     ViteImageOptimizer({
-      png: {
-        quality: 86,
-      },
-      jpeg: {
-        quality: 86,
-      },
-      jpg: {
-        quality: 86,
-      },
+      png: { quality: 86 },
+      jpeg: { quality: 86 },
+      jpg: { quality: 86 },
     }),
     {
-      ...imagemin(["./src/img/**/*.{jpg,png,jpeg}"], {
-        destination: "./src/img/webp/",
+      ...imagemin(['./src/img/**/*.{jpg,png,jpeg}'], {
+        destination: './src/img/webp/',
         plugins: [imageminWebp({ quality: 86 })],
       }),
-      apply: "serve",
+      apply: 'serve',
     },
   ],
+
   build: {
-    minify: false, // disable minification
+    minify: false,
     rollupOptions: {
       input: Object.fromEntries(
         glob
-          .sync(["./*.html", "./pages/**/*.html"])
-          .map((file) => [
-            path.relative(__dirname, file.slice(0, file.length - path.extname(file).length)),
+          .sync(['./*.html', './pages/**/*.html'])
+          .map(file => [
+            path.relative(
+              __dirname,
+              file.slice(0, file.length - path.extname(file).length)
+            ),
             fileURLToPath(new URL(file, import.meta.url)),
           ])
       ),
-      // output unminified CSS file
       output: {
-        assetFileNames: "assets/[name].[ext]",
+        assetFileNames: 'assets/[name].[ext]',
       },
     },
   },
